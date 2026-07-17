a3e0d2fadd65a1bb02d7ed6ce752b1805bdbe448
Karandeep Singh <karan@Navdeeps-MacBook-Air.local>
2026-07-17 13:10:14 -0700

    implemted warning count and configuration file usage

--- health_monitor.sh
+++ health_monitor.sh
@@ -3,16 +3,25 @@
 LOG_FILE="health_report.log"
 exec >> "$LOG_FILE" 2>&1
 
-echo "=========================="
-echo " Server Health Report"
-echo "=========================="
+if [ -f config.conf ] 
+then
+    echo "ERROR: config.conf not found"
+    exit 1
+fi
+source config.conf
 
-
-HOSTNAME=${hostname}
-CURRENT_DATE=${date}
+echo "=========================="
+echo " New Health Check Run"
+echo "=========================="
+echo "Run Time $(date +"%A, %B %d, %Y %I:%M:%S %p")"
+echo "Run Time: $(date +"%A, %B %d, %Y %I:%M:%S %p")"
+HOSTNAME=$(hostname)
+CURRENT_DATE=$(date)
 
 echo "Hostname: $HOSTNAME"
 echo "Date: $CURRENT_DATE"
+
+WARNING_COUNT=0
 
 
 check_threshold() {
@@ -22,22 +31,22 @@ check_threshold() {
 
     echo "$metric Usage: ${value}%"
 
-    if [ "$value" -gt "$threshold" ] 
+    if [ "$value" -ge "$threshold" ] 
     then
     echo "WARNING: $metric usage above ${threshold}%"
+    WARNING_COUNT=$((WARNING_COUNT+1))
     fi
+
 }
 
 CPU_IDLE=$(top -bn1 | grep "Cpu" | awk '{print $8}')
-CPU_USAGE=$(echo "100 - $CPU_IDLE" | bc | awk '{printf "%.0f", %1'})
-CPU_THRESHOLD=80
+CPU_USAGE=$(echo "100 - $CPU_IDLE" | bc | awk '{printf "%.0f", $1}')
 
 
 MEMORY_USAGE=$(free | awk '/Mem/ {printf "%.0f", $3/$2 * 100}')
-MEMORY_THRESHOLD=80
+
 
 DISK_USAGE=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
-DISK_THRESHOLD=80
 
 
 check_threshold "Memory"  "$MEMORY_USAGE" "$MEMORY_THRESHOLD"
@@ -60,4 +69,16 @@ then
     echo "Network: OK"
 else
     echo "Network: FAILED"
+fi
+
+echo ""
+echo "Total Warnings: ${WARNING_COUNT}"
+
+
+
+if [ "$WARNING_COUNT" -gt 0 ]
+then
+    exit 1
+else
+    exit 0
 fi
